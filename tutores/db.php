292538c403d373255519f6a18cb4a1be1c2a53cc
JaimeBucarey19 <bucarey19jaime@hotmail.com>
2022-05-25 15:20:42 -0400

    Coomit de tutores crud vistas

---
+++ tutores/db.php
@@ -0,0 +1,16 @@
+<?php
+$conn=mysqli_connect(
+  'localhost',
+  'root',
+  '',
+  'login_desarrollo'
+);
+
+
+// por si la base de datos esta conectada
+// if(isset($conn)) {
+//     echo 'base de datos conectada';
+// }
+
+
+?>
