a7f0f24afd283ed5730804abd1a0fcf0d1ac24c8
JaimeBucarey19 <bucarey19jaime@hotmail.com>
2022-05-29 02:50:23 -0400

    Commit tutores terminado

--- tutores/includes/header.php
+++ tutores/includes/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="es">
   <head>
-    <title>CRUD </title>
+    <title>Tutores </title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,7 +15,7 @@
           <div class="row">
               <div class="col-md">
                   <header class="py-3">
-                      <h3 class="text-center">CRUD </h3>
+                      <h3 class="text-center">Tutores </h3>
                   </header>
               </div>
           </div>
